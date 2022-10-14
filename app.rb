755cf58657f36b23922706909671292ce602db1d
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-14 23:46:37 +0300

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,27 @@
+# app.rb
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
+
+# print "Hello, world!"
+# print "Hello, world!"
+# print "Hello, world!"
+# print "Hello, world!"
+
+# Thses two are both similar
+# puts [1, 2, 3].inspect
+# p [1, 2, 3]
+
+# pp [{
+#      id: 1,
+#      hello: "Hello, world!",
+#    }, {
+#      id: 2,
+#      hello: "Hello, world!",
+#    }, {
+#      id: 3,
+#      hello: "Hello, world!",
+#    }]
+
+# p "Welcome to the world!"
